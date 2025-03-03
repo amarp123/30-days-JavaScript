03a78cecfc2d8cd1e8f0d8f9279e2ff8dcb18361
Amar Pujari <amarpujari800@gmail.com>
2025-03-04 00:07:11 +0530

    Update Variables.js

--- Variables.js
+++ Variables.js
@@ -1,17 +1,82 @@
-//var keyword
-function test() {
-    var a = "3";
-    var b = "8";
+function lifeInWeeks(age) {
     
-/***********Do not change the code above 👆*******/
-//Write your code on lines 7 - 9:
-a="8";
-b="3";
-
+    /************Don't change the code above************/    
+        
+        //Write your code here.
+        var x=(90-age)*365;
+        var y=(90-age)*52;
+        var z=(90-age)*12;
+        console.log("You have "+x+" days, "+y+" weeks, and "+z+" months left.");
+        
+        
+        
+        
+        
+    /*************Don't change the code below**********/
+    }
+    lifeInWeeks(56);
     
-/***********Do not change the code below 👇*******/
+//whos buy lunch challange
 
-    console.log("a is " + a);
-    console.log("b is " + b);
+function whosPaying(names) {
+    
+    
+            var numberOfpeople=names.length;
+            var randomPosition=Math.floor(Math.random()*numberOfpeople);
+            var randomperson=names[randomPosition];
+            
+            return randomperson+" is going to buy lunch today!"
+        
+    }    
+
+
+
+    //leap year challenge
+
+    function isLeap(year) {
+    
+        /**************Don't change the code above****************/    
+            
+            //Write your code here.   
+            if((year%4===0&&year%100!=0)||(year%4==0&&year%100!=0&&year%400===0)){
+                return console.log("Leap year.");
+            }
+            
+            else{
+                return console.log("Not leap year.");
+            }
+        
+        
+            
+        
+        /**************Don't change the code below****************/    
+        
+        }
+        isLeap(1989);
+
+   //fibonacci challenge
+
+  function fibonacciGenerator (n) {
+//Do NOT change any of the code above 👆
+    
+    //Write your code here:
+    
+    var output=[];
+    if(n===1){
+        output=[0];
+        
+    }
+    else if(n===2){
+        output=[0,1];
+    }
+    else{
+        output=[0,1];
+        for(var i=2;i<n;i++){
+            output.push(output[output.length-2]+output[output.length-1]);
+        }
+    }
+    return output;
 }
+
+fibonacciGenerator(1);
 
